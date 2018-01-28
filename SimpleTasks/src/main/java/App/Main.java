12abfd8a0c422b60a32c5761abfe752480093266
Kamil <kamil4525@gmail.com>
2018-01-28 21:32:11 +0100

    Project setup

---
+++ SimpleTasks/src/main/java/App/Main.java
@@ -0,0 +1,18 @@
+package App;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		System.out.println("HELLO");
+		
+	}
+
+}
